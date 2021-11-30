fbaa89eb94d59d496cced80f06e43620e37d68f3
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2021-11-30 13:46:22 +1100

    Stage 6/6 Trigrams Based

--- text_generator.py
+++ text_generator.py
@@ -22,14 +22,16 @@ class NotInModelError(Exception):
 
 
 class TextPredictor:
-    def __init__(self, filename, pattern, sents, words):
+    def __init__(self, filename, pattern, sents, words, depth=3):
         self.filename = filename
         self.pattern = pattern
         self.tokens = None
         self.bigrams = None
+        self.trigrams = None
         self.head_tails = None
         self.sents = sents
         self.words = words
+        self.depth = depth
 
     def get_tokens(self):
         with open(self.filename, 'r', encoding="utf-8") as file:
@@ -37,23 +39,32 @@ class TextPredictor:
         return regexp_tokenize(text, self.pattern)
 
     def get_bigrams(self):
-        return [[self.tokens[i], self.tokens[i + 1]] for i in range(len(self.tokens) - 1)]
+        return [
+            [self.tokens[i], self.tokens[i + 1]]
+            for i in range(len(self.tokens) - 1)]
+
+    def get_trigrams(self):
+        return [
+            [' '.join((self.tokens[i], self.tokens[i + 1])), self.tokens[i + 2]]
+            for i in range(len(self.tokens) - 2)]
 
     def get_collection_stats(self, collection: list) -> 'string with # of elements in collection':
         """Get collection statistics"""
-        if len(collection[0]) == 2:  # collection is bigram list
-            return f'Number of bigrams: {len(self.bigrams)}'
+        if len(collection[0]) == 2:
+            if len(collection[0].split()) == 2:
+                return f'Number of trigrams: {len(collection)}' # collection is trigrams list
+            return f'Number of bigrams: {len(collection)}'  # collection is bigrams list
         else:  # collection is tokens list
-            total_tokens, unique_tokens = len(self.tokens), len(set(self.tokens))
+            total_tokens, unique_tokens = len(collection), len(set(collection))
             return f'Corpus statistics\nAll tokens: {total_tokens}\nUnique tokens: {unique_tokens}'
 
     def get_element_by_ind(self, ind: str, collection: list) -> 'string or list from collection':
-        """Pull token or bigram from collection by index"""
+        """Pull token or basis from collection by index"""
         if match(r'^-?\d+$', ind) is None:
             raise NonIntError
         if int(ind) > len(collection) - 1 or int(ind) < 0 and abs(int(ind)) > len(collection):
             raise RangeError
-        if len(collection[0]) == 2:  # collection is bigram list
+        if len(collection[0]) == 2:  # collection is basis list
             return f'Head: {collection[int(ind)][0]} Tail: {collection[int(ind)][1]}'
         else:  # collection is tokens list
             return collection[int(ind)]
@@ -61,8 +72,12 @@ class TextPredictor:
     def get_head_tails(self):
         """Make dictionary with key head and value tails"""
         my_dict = defaultdict(list)
-        for bigram in self.bigrams:
-            head, tail = bigram[0], bigram[1]
+        if self.depth == 2:
+            basis = self.get_bigrams()
+        if self.depth == 3:
+            basis = self.get_trigrams()
+        for pair in basis:
+            head, tail = pair[0], pair[1]
             my_dict[head].append(tail)
         return my_dict
 
@@ -84,32 +99,37 @@ class TextPredictor:
     def get_sentences(self):
         """Make specified num of sentences with not less than specified num of words"""
         sentences = []
-        starters = {word for word in self.head_tails.keys() if match(r'^[A-Z].*[^.?!]$', word)}
+        starters = {el for el in self.head_tails.keys() if match(r'^[A-Z].*[^.?!]$', el.split()[0])}
         for head in [random.choice(list(starters)) for _ in range(n_sentences)]:
-            sentence = [head]
+            sentence = [word for word in head.split()]
             while len(sentence) < self.words or sentence[-1][-1] not in '.?!':  # match(r'.+[.?!]$', sentence[-1]) is None
                 tails_freq = Counter(self.head_tails[head])
                 tails = [tail for tail in tails_freq]
                 weights = [tails_freq[tail] for tail in tails]
                 next_word = ''.join(random.choices(tails, weights))
                 sentence.append(next_word)
-                head = next_word
+                if self.depth == 2:
+                    head = sentence[-1]
+                if self.depth == 3:
+                    head = ' '.join((sentence[-2], sentence[-1]))
             sentences.append(' '.join(sentence))
         return '\n'.join(sentences)
 
     def main(self):
-        start = perf_counter()
+        # start = perf_counter()
         self.tokens = self.get_tokens()
-        self.bigrams = self.get_bigrams()
         self.head_tails = self.get_head_tails()
-        print(self.get_sentences())
-        end = perf_counter()
+        result = self.get_sentences()
+        print(result)
+        # end = perf_counter()
+        # print(end - start)
 
 corpus = input()
 tokenizer_pattern = r'\S+'
 n_sentences, n_words = 10, 5
 
-my_text_app = TextPredictor(corpus, \
-                            tokenizer_pattern, \
-                            n_sentences, n_words)
+my_text_app = TextPredictor(corpus,
+                            tokenizer_pattern,
+                            n_sentences, n_words,
+                            depth=3)
 my_text_app.main()
